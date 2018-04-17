ec860383c826a803e14b9df6545d5266c9f7fa09
homyakny1 <homyakny@gmail.com>
2018-04-17 00:28:21 -0400

    trying to fix heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3033;
 const models = require("./models");
 
 const passport = require("passport");
