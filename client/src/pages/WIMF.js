311ed2f4e3f5a49180d0e40519db5643089ba6b5
Eugene Khom <homyakny@gmail.com>
2018-06-12 22:44:41 -0400

    added whats in my fridge

--- client/src/pages/WIMF.js
+++ client/src/pages/WIMF.js
@@ -7,7 +7,9 @@ import './style.css';
 const WIMF = (props) => (
     <div>
         <Navbar handleLogout ={props.handleLogout} />
-            <img id='fridge-photo' src='https://3.bp.blogspot.com/-92t-6yhZFvI/WlQ8pBMwS1I/AAAAAAAAkMM/dnR6rJjD5BQg8coHrZlgWDM-ixPG7hK3gCLcBGAs/s1600/what%2527s%2Bin%2Bmy%2Bfridge%2Bhow%2Bi%2Borganize%2Bmy%2Bfridge-2.jpg' alt='fridge'/>
+        <div className ='center'>
+            <img id='fridge-photo' src='http://homyakny1.epizy.com/fridge.jpg' alt='fridge'/>
+        </div>
         <Footer/>
     </div>
 );
